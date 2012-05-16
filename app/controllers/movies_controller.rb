52a0e68ba95a9bb4e5246b1fe8080568959e4130
Jason Whitehorn <jason.whitehorn@gmail.com>
2012-05-15 21:06:49 -0500

    just enough to update the movie model

--- app/controllers/movies_controller.rb
+++ app/controllers/movies_controller.rb
@@ -1,5 +1,13 @@
 class MoviesController < ApplicationController
   
+  expose :movie
   expose (:movies) { Movie.order(:name).all }
   
+  
+  def update
+    movie.update_attributes params[:movie]
+    
+    flash[:notice] = "Movie Updated"
+    redirect_to :back
+  end
 end
