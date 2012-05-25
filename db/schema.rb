a745ae2a89991d32701a71d032bb0f2ff1f34ed9
Jason Whitehorn <jason.whitehorn@gmail.com>
2012-05-24 21:13:27 -0500

    add a show model

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120519060547) do
+ActiveRecord::Schema.define(:version => 20120521000121) do
 
   create_table "delayed_jobs", :force => true do |t|
     t.integer  "priority",   :default => 0
@@ -42,6 +42,12 @@ ActiveRecord::Schema.define(:version => 20120519060547) do
     t.integer  "coverart_file_size"
     t.datetime "coverart_updated_at"
     t.integer  "play_count"
+  end
+
+  create_table "shows", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
   end
 
 end
